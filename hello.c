0f57844010f120dae517f000c0dff3bca143be5d
liyixin01 <1094833252@qq.com>
2019-07-28 20:57:42 +0800

    提交代码

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2019年07月28日 20时26分50秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  liyixin (), 1094833252@qq.com
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc ,char *argv[])
+{
+	printf("hello world\n");
+}
